210b2b76c1ddd495132e793f5ebb7f0216da5e24
Igor Thierry <igorthierry15@gmail.com>
2020-04-22 12:16:48 -0300

    desafio completo

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -2,3 +2,8 @@ const formatValue = (value: number): string =>
   Intl.NumberFormat().format(value); // TODO
 
 export default formatValue;
+
+export const { format: formatPrice } = new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+});
